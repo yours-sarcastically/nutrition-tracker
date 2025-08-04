84b6cdaf0e1ba0ab16651dc776b082c5f336c681
yours-sarcastically <surya.shivaprasad18@gmail.com>
2025-08-04 02:24:51 +0200

    Create __init__.py

---
+++ src/ui/__init__.py
@@ -0,0 +1,6 @@
+"""UI components for the nutrition tracker application."""
+
+from .components import UIComponents
+from .main import NutritionTrackerUI
+
+__all__ = ['UIComponents', 'NutritionTrackerUI']
