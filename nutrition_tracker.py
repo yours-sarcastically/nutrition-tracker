a32732eab5762b880d1c15af27d4a09e28ed46da
yours-sarcastically <surya.shivaprasad18@gmail.com>
2025-08-09 02:11:11 +0200

    Update nutrition_tracker.py

--- nutrition_tracker.py
+++ nutrition_tracker.py
@@ -1035,7 +1035,7 @@ foods = assign_food_emojis(foods)
 st.markdown("""
 <style>
 html {
-    font-size: 150%; /* Makes all text 10% larger */
+    font-size: 110%; /* Makes all text 10% larger */
 }
 
 [data-testid="InputInstructions"] { display: none; }
